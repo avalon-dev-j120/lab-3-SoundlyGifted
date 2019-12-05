1047e73034e671f6de74f886d85ca74151ee2c39
SoundlyGifted <Raven131313@yandex.ru>
2019-12-05 20:01:33 +0300

    DEV-J120 APP1 commit 1

---
+++ DEVJ120_LAB3_APP1/src/devj120_lab3_app1/DEVJ120_LAB3_APP1.java
@@ -0,0 +1,14 @@
+
+package devj120_lab3_app1;
+
+public class DEVJ120_LAB3_APP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ColorPickerWindow colorPickerWindow = new ColorPickerWindow();
+        colorPickerWindow.setVisible(true); 
+    }
+    
+}
